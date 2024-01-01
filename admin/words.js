5bb6edba6ce9e208d98d968956ad00d1a3faa52d
ioBroker Adapter Creator <noreply@iobroker.net>
2024-01-01 20:36:54 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,31 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+"use strict";
+
+systemDictionary = {
+	"smart-race-adapter adapter settings": {
+		"en": "Adapter settings for smart-race-adapter",
+		"de": "Adaptereinstellungen für smart-race-adapter",
+		"ru": "Настройки адаптера для smart-race-adapter",
+		"pt": "Configurações do adaptador para smart-race-adapter",
+		"nl": "Adapterinstellingen voor smart-race-adapter",
+		"fr": "Paramètres d'adaptateur pour smart-race-adapter",
+		"it": "Impostazioni dell'adattatore per smart-race-adapter",
+		"es": "Ajustes del adaptador para smart-race-adapter",
+		"pl": "Ustawienia adaptera dla smart-race-adapter",
+		"zh-cn": "smart-race-adapter的适配器设置"
+	},
+	"port": {
+		"en": "port",
+		"de": "port",
+		"ru": "port",
+		"pt": "port",
+		"nl": "port",
+		"fr": "port",
+		"it": "port",
+		"es": "port",
+		"pl": "port",
+		"zh-cn": "port"
+	}
+};
