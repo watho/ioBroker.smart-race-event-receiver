04ff4950940916929b4f6920a1e92515aa9146a3
watho <watho@users.noreply.github.com>
2024-01-14 13:27:25 +0100

    Added es-lint plugin for configuring line breaks.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,11 @@ module.exports = {
         "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
-    plugins: [],
+    plugins: [
+        '@stylistic'
+    ],
     rules: {
+        //"@stylistic/linebreak-style": ["error", "windows"],
         "@typescript-eslint/no-parameter-properties": "off",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-use-before-define": [
@@ -42,7 +45,6 @@ module.exports = {
         "no-var": "error",
         "prefer-const": "error",
         "no-trailing-spaces": "error",
-        "linebreak-style": ["error", "CRLF"]
     },
     overrides: [
         {
